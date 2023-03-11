e3034a4b9fd81fbd0ab7935df761b613689a68cf
soufiyane <soufiyane.aitmoulay@gmail.com>
2023-03-11 19:43:54 +0100

    Shopping-Cart

--- script.js
+++ script.js
@@ -1,80 +1,109 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let bntPlus = document.querySelectorAll(".btn-plus");
-let bntMinus = document.querySelectorAll(".btn-minus");
-let QuantiteValue = document.querySelectorAll(".quantite-input");
-let btnLike = document.querySelectorAll(".btn-like");
-let btnDelete = document.querySelectorAll(".btn-delete");
-let CardsElements = document.querySelectorAll(".card");
-let Prices = document.querySelectorAll(".initial-price");
-let ChangeblePrices = document.querySelectorAll(".changeble-price");
-let TotalPrice = document.querySelector(".total");
-let TotalTaxed = document.querySelector(".TotalTaxed");
-let removePopup = document.querySelector(".remove-popup");
-let modalPopup = document.querySelector(".pop-up");
-let popupContainer = document.querySelector(".popup-container");
-let processBtn = document.querySelector(".processBtn");
-function CalculTotal() {
-    ChangeblePrices = document.querySelectorAll(".changeble-price");
-    var Total = 0;
-    ChangeblePrices.forEach((e) => {
-        Total = parseFloat((Total + parseFloat(e.innerText)).toFixed(2));
-        TotalPrice.innerText = Total.toString();
-    });
-}
-function CalcultotalTaxed() {
-    var HTprice = (parseFloat(TotalPrice.innerText) - 14.00);
-    TotalTaxed.innerText = HTprice.toFixed(2).toString();
-}
-function processCalcul() {
-    return (parseFloat(TotalPrice.innerText) - 14.00);
-}
-bntMinus.forEach((btn, i) => {
-    btn.addEventListener("click", (e) => {
-        e.preventDefault();
-        if (parseInt(QuantiteValue[i].value) != 1) {
-            QuantiteValue[i].value = (parseInt(QuantiteValue[i].value) - 1).toString();
+exports.__esModule = true;
+var leftCart = document.querySelector(".left-cart");
+var removePopup = document.querySelector(".remove-popup");
+var modalPopup = document.querySelector(".pop-up");
+var popupContainer = document.querySelector(".popup-container");
+var processBtn = document.querySelector(".processBtn");
+var request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+    if (request.readyState === XMLHttpRequest.DONE) {
+        if (request.status === 200) {
+            var data = JSON.parse(request.responseText);
+            displayCards(data);
         }
-        ChangeblePrices[i].innerText = (parseFloat(Prices[i].innerText) * parseFloat(QuantiteValue[i].value)).toFixed(2).toString();
-        CalculTotal();
-        CalcultotalTaxed();
-    });
-});
-bntPlus.forEach((btn, i) => {
-    btn.addEventListener("click", (e) => {
-        e.preventDefault();
-        QuantiteValue[i].value = (parseInt(QuantiteValue[i].value) + 1).toString();
-        ChangeblePrices[i].innerText = (parseFloat(Prices[i].innerText) * parseFloat(QuantiteValue[i].value)).toFixed(2).toString();
-        CalculTotal();
-        CalcultotalTaxed();
-    });
-});
-btnLike.forEach((e, i) => {
-    e.addEventListener("click", (c) => {
-        c.preventDefault();
-        e.classList.toggle("like");
-    });
-});
-btnDelete.forEach((e, i) => {
-    e.addEventListener("click", (c) => {
-        c.preventDefault();
-        CardsElements[i].remove();
-        CalculTotal();
-        CalcultotalTaxed();
-    });
-});
-removePopup.addEventListener("click", (e) => {
-    e.preventDefault();
-    modalPopup.classList.add("remove");
-    popupContainer.classList.add("remove");
-    function remove() {
-        modalPopup.remove();
-        popupContainer.remove();
+        else {
+            console.error("Request failed with status ".concat(request.status));
+        }
     }
-    setTimeout(remove, 1000);
+};
+request.open('GET', 'data.json', true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send();
+window.addEventListener("load", function () {
+    var bntPlus = document.querySelectorAll(".btn-plus");
+    var bntMinus = document.querySelectorAll(".btn-minus");
+    var QuantiteValue = document.querySelectorAll(".quantite-input");
+    var btnLike = document.querySelectorAll(".btn-like");
+    var btnDelete = document.querySelectorAll(".btn-delete");
+    var CardsElements = document.querySelectorAll(".card");
+    var Prices = document.querySelectorAll(".initial-price");
+    var ChangeblePrices = document.querySelectorAll(".changeble-price");
+    var TotalPrice = document.querySelector(".total");
+    var TotalTaxed = document.querySelector(".TotalTaxed");
+    function CalculTotal() {
+        ChangeblePrices = document.querySelectorAll(".changeble-price");
+        var Total = 0;
+        ChangeblePrices.forEach(function (e) {
+            Total = parseFloat((Total + parseFloat(e.innerText)).toFixed(2));
+            TotalPrice.innerText = Total.toString();
+        });
+    }
+    function CalcultotalTaxed() {
+        var HTprice = (parseFloat(TotalPrice.innerText) - 14.00);
+        TotalTaxed.innerText = HTprice.toFixed(2).toString();
+    }
+    function processCalcul() {
+        return (parseFloat(TotalPrice.innerText) - 14.00);
+    }
+    bntMinus.forEach(function (btn, i) {
+        btn.addEventListener("click", function (e) {
+            e.preventDefault();
+            if (parseInt(QuantiteValue[i].value) != 1) {
+                QuantiteValue[i].value = (parseInt(QuantiteValue[i].value) - 1).toString();
+            }
+            ChangeblePrices[i].innerText = (parseFloat(Prices[i].innerText) * parseFloat(QuantiteValue[i].value)).toFixed(2).toString();
+            CalculTotal();
+            CalcultotalTaxed();
+        });
+    });
+    bntPlus.forEach(function (btn, i) {
+        btn.addEventListener("click", function (e) {
+            e.preventDefault();
+            QuantiteValue[i].value = (parseInt(QuantiteValue[i].value) + 1).toString();
+            ChangeblePrices[i].innerText = (parseFloat(Prices[i].innerText) * parseFloat(QuantiteValue[i].value)).toFixed(2).toString();
+            CalculTotal();
+            CalcultotalTaxed();
+        });
+    });
+    btnLike.forEach(function (e, i) {
+        e.addEventListener("click", function (c) {
+            c.preventDefault();
+            e.classList.toggle("like");
+        });
+    });
+    btnDelete.forEach(function (e, i) {
+        e.addEventListener("click", function (c) {
+            c.preventDefault();
+            CardsElements[i].remove();
+            CalculTotal();
+            CalcultotalTaxed();
+        });
+    });
+    removePopup.addEventListener("click", function (e) {
+        e.preventDefault();
+        modalPopup.classList.add("remove");
+        popupContainer.classList.add("remove");
+        function remove() {
+            modalPopup.remove();
+            popupContainer.remove();
+        }
+        setTimeout(remove, 1000);
+    });
+    CalculTotal();
+    CalcultotalTaxed();
+    processBtn.addEventListener("click", function (e) {
+        alert("your Total is ".concat(processCalcul().toFixed(2)));
+    });
 });
-CalculTotal();
-CalcultotalTaxed();
-processBtn.addEventListener("click", (e) => {
-    alert(`your Total is ${processCalcul().toFixed(2)}`);
-});
+function displayCards(data) {
+    var html = "";
+    data.forEach(function (e, i) {
+        html += "\n        <div class=\"card\">\n            <div class=\"left-card\">\n                <img src=\"".concat(e.image, "\"/>\n            </div>\n            <div class=\"right-card\">\n                <div class=\"right-card-top\">\n                    <div class=\"right-card-top-left\">\n                        <h3>").concat(e.name, "</h3>\n                        <p>\n                            <span class=\"initial-price\">").concat(e.price, "$</span>  \n                            <span style=\"color: #282828;\">|</span> \n                            <span style=\"color: #54B435;\">In Stock</span>\n                        </p>\n                    </div>\n                    <div class=\"right-card-top-right \">\n                        <span class=\"changeble-price\">").concat(e.price, "</span>$\n                    </div>\n                </div>\n                <div class=\"right-card-bottom\">\n                    <div class=\"right-card-bottom-left\">\n                        <select class=\"sizes\" name=\"sizes\">\n                            <option selected disabled>size</option>\n                            ").concat(e.sizes.map(function (size) {
+            return "<option value=\"".concat(size, "\">").concat(size, "</option>");
+        }), "\n                        </select>\n                        <select class=\"colors\" name=\"colors\">\n                            <option selected disabled>Colors</option>\n                            ").concat(e.colors.map(function (color) {
+            return "<option value=\"".concat(color, "\">").concat(color, "</option>");
+        }), "\n                        </select>\n                        <div class=\"quantite\">\n                            <button class=\"btn-minus\">-</button>\n                            <input class=\"quantite-input\" type=\"text\" value=\"1\" disabled >\n                            <button class=\"btn-plus\">+</button>\n                        </div>\n                    </div>\n                    <div class=\"right-card-bottom-right\">\n                        <button class=\"btn-like\"><i class=\"fa-solid fa-heart\"></i> Save</button>\n                        <button class=\"btn-delete\"><i class=\"fa-solid fa-trash\"></i> Delete</button>\n                    </div>\n                </div>\n            </div>\n         </div>");
+    });
+    leftCart.insertAdjacentHTML("beforeend", html);
+}
--- script.ts
+++ script.ts
@@ -1,91 +1,165 @@
 export {};
-
-let bntPlus = document.querySelectorAll(".btn-plus") as NodeListOf<HTMLButtonElement> ;
-let bntMinus = document.querySelectorAll(".btn-minus") as  NodeListOf<HTMLButtonElement> ;
-let QuantiteValue = document.querySelectorAll(".quantite-input") as NodeListOf<HTMLInputElement> ;
-let btnLike  = document.querySelectorAll(".btn-like") as NodeListOf<HTMLButtonElement>
-let btnDelete  = document.querySelectorAll(".btn-delete") as NodeListOf<HTMLButtonElement>
-let CardsElements  = document.querySelectorAll(".card") as NodeListOf<HTMLDivElement>
-let Prices = document.querySelectorAll(".initial-price") as NodeListOf<HTMLSpanElement>
-let ChangeblePrices = document.querySelectorAll(".changeble-price") as NodeListOf<HTMLSpanElement>
-let TotalPrice = document.querySelector(".total") as HTMLSpanElement;
-let TotalTaxed = document.querySelector(".TotalTaxed") as HTMLSpanElement
+let leftCart = document.querySelector(".left-cart") as HTMLDivElement
 let removePopup = document.querySelector(".remove-popup") as HTMLButtonElement
 let modalPopup = document.querySelector(".pop-up") as HTMLDivElement
 let popupContainer = document.querySelector(".popup-container") as HTMLDivElement
 let processBtn = document.querySelector(".processBtn") as HTMLButtonElement
 
-function CalculTotal():void{
-    ChangeblePrices = document.querySelectorAll(".changeble-price") as NodeListOf<HTMLSpanElement>
-    var Total:number = 0;  
-    ChangeblePrices.forEach((e)=>{
-        Total = parseFloat((Total + parseFloat(e.innerText)).toFixed(2));
-        TotalPrice.innerText = Total.toString();
+const request = new XMLHttpRequest();
+request.onreadystatechange = () => {
+    if (request.readyState === XMLHttpRequest.DONE) {
+    if (request.status === 200) {
+      const data = JSON.parse(request.responseText);
+        displayCards(data);
+    } 
+    else {
+      console.error(`Request failed with status ${request.status}`);
+    }
+  }
+};
+
+request.open('GET', 'data.json', true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send();
+
+
+window.addEventListener("load",()=>{
+    let bntPlus = document.querySelectorAll(".btn-plus") as NodeListOf<HTMLButtonElement> ;
+    let bntMinus = document.querySelectorAll(".btn-minus") as  NodeListOf<HTMLButtonElement> ;
+    let QuantiteValue = document.querySelectorAll(".quantite-input") as NodeListOf<HTMLInputElement> ;
+    let btnLike  = document.querySelectorAll(".btn-like") as NodeListOf<HTMLButtonElement>
+    let btnDelete  = document.querySelectorAll(".btn-delete") as NodeListOf<HTMLButtonElement>
+    let CardsElements  = document.querySelectorAll(".card") as NodeListOf<HTMLDivElement>
+    let Prices = document.querySelectorAll(".initial-price") as NodeListOf<HTMLSpanElement>
+    let ChangeblePrices = document.querySelectorAll(".changeble-price") as NodeListOf<HTMLSpanElement>
+    let TotalPrice = document.querySelector(".total") as HTMLSpanElement;
+    let TotalTaxed = document.querySelector(".TotalTaxed") as HTMLSpanElement
+    function CalculTotal():void{
+        ChangeblePrices = document.querySelectorAll(".changeble-price") as NodeListOf<HTMLSpanElement>
+        var Total:number = 0;  
+        ChangeblePrices.forEach((e)=>{
+            Total = parseFloat((Total + parseFloat(e.innerText)).toFixed(2));
+            TotalPrice.innerText = Total.toString();
+        })
+    }
+    
+    function CalcultotalTaxed():void{
+        var HTprice:number = (parseFloat(TotalPrice.innerText)-14.00);
+        TotalTaxed.innerText = HTprice.toFixed(2).toString();
+    }
+    
+    function processCalcul():number{
+        return (parseFloat(TotalPrice.innerText)-14.00);
+    }
+    
+    bntMinus.forEach((btn,i)=>{
+        btn.addEventListener("click",(e)=>{
+            e.preventDefault();
+            if(parseInt(QuantiteValue[i].value)!=1){
+                QuantiteValue[i].value = (parseInt(QuantiteValue[i].value) - 1).toString();
+            }
+            ChangeblePrices[i].innerText = (parseFloat(Prices[i].innerText)*parseFloat(QuantiteValue[i].value)).toFixed(2).toString();
+            CalculTotal();
+            CalcultotalTaxed();
+        })
     })
-}
-
-function CalcultotalTaxed():void{
-    var HTprice:number = (parseFloat(TotalPrice.innerText)-14.00);
-    TotalTaxed.innerText = HTprice.toFixed(2).toString();
-}
-
-function processCalcul():number{
-
-    return (parseFloat(TotalPrice.innerText)-14.00);
-}
-
-bntMinus.forEach((btn,i)=>{
-    btn.addEventListener("click",(e)=>{
+    
+    bntPlus.forEach((btn,i)=>{
+        btn.addEventListener("click",(e)=>{
+            e.preventDefault();
+            QuantiteValue[i].value = (parseInt(QuantiteValue[i].value) + 1).toString();     
+            ChangeblePrices[i].innerText = (parseFloat(Prices[i].innerText)*parseFloat(QuantiteValue[i].value)).toFixed(2).toString();    
+            CalculTotal()
+            CalcultotalTaxed();
+        })
+        
+    })
+    
+    btnLike.forEach((e,i)=>{
+        e.addEventListener("click",(c)=>{
+            c.preventDefault();
+            e.classList.toggle("like")
+        })
+    })
+    
+    btnDelete.forEach((e,i)=>{
+        e.addEventListener("click",(c)=>{
+            c.preventDefault();
+            CardsElements[i].remove();
+            CalculTotal();
+            CalcultotalTaxed();
+        })
+    })
+    
+    removePopup.addEventListener("click",(e)=>{
         e.preventDefault();
-        if(parseInt(QuantiteValue[i].value)!=1){
-            QuantiteValue[i].value = (parseInt(QuantiteValue[i].value) - 1).toString();
+        modalPopup.classList.add("remove")
+        popupContainer.classList.add("remove")
+        function remove(){
+            modalPopup.remove();
+            popupContainer.remove();
         }
-        ChangeblePrices[i].innerText = (parseFloat(Prices[i].innerText)*parseFloat(QuantiteValue[i].value)).toFixed(2).toString();
-        CalculTotal();
-        CalcultotalTaxed();
+        setTimeout(remove,1000);
     })
-})
-
-bntPlus.forEach((btn,i)=>{
-    btn.addEventListener("click",(e)=>{
-        e.preventDefault();
-        QuantiteValue[i].value = (parseInt(QuantiteValue[i].value) + 1).toString();     
-        ChangeblePrices[i].innerText = (parseFloat(Prices[i].innerText)*parseFloat(QuantiteValue[i].value)).toFixed(2).toString();    
-        CalculTotal()
-        CalcultotalTaxed();
-    })
+    
+    CalculTotal();
+    CalcultotalTaxed();
+    processBtn.addEventListener("click",(e)=>{
+        alert(`your Total is ${processCalcul().toFixed(2)}`)
+    });
     
 })
 
-btnLike.forEach((e,i)=>{
-    e.addEventListener("click",(c)=>{
-        c.preventDefault();
-        e.classList.toggle("like")
-    })
-})
+function displayCards(data:{image:String,name:String,price:Number,sizes:String[],colors:String[]}[]): void{
 
-btnDelete.forEach((e,i)=>{
-    e.addEventListener("click",(c)=>{
-        c.preventDefault();
-        CardsElements[i].remove();
-        CalculTotal();
-        CalcultotalTaxed();
-    })
-})
-
-removePopup.addEventListener("click",(e)=>{
-    e.preventDefault();
-    modalPopup.classList.add("remove")
-    popupContainer.classList.add("remove")
-    function remove(){
-        modalPopup.remove();
-        popupContainer.remove();
-    }
-    setTimeout(remove,1000);
-})
-
-CalculTotal();
-CalcultotalTaxed();
-processBtn.addEventListener("click",(e)=>{
-    alert(`your Total is ${processCalcul().toFixed(2)}`)
-})
+    var html = "";
+    data.forEach((e,i)=>{
+        html += `
+        <div class="card">
+            <div class="left-card">
+                <img src="${e.image}"/>
+            </div>
+            <div class="right-card">
+                <div class="right-card-top">
+                    <div class="right-card-top-left">
+                        <h3>${e.name}</h3>
+                        <p>
+                            <span class="initial-price">${e.price}$</span>  
+                            <span style="color: #282828;">|</span> 
+                            <span style="color: #54B435;">In Stock</span>
+                        </p>
+                    </div>
+                    <div class="right-card-top-right ">
+                        <span class="changeble-price">${e.price}</span>$
+                    </div>
+                </div>
+                <div class="right-card-bottom">
+                    <div class="right-card-bottom-left">
+                        <select class="sizes" name="sizes">
+                            <option selected disabled>size</option>
+                            ${e.sizes.map((size)=>{
+                               return `<option value="${size}">${size}</option>`
+                            })}
+                        </select>
+                        <select class="colors" name="colors">
+                            <option selected disabled>Colors</option>
+                            ${e.colors.map((color)=>{
+                                return `<option value="${color}">${color}</option>`
+                            })}
+                        </select>
+                        <div class="quantite">
+                            <button class="btn-minus">-</button>
+                            <input class="quantite-input" type="text" value="1" disabled >
+                            <button class="btn-plus">+</button>
+                        </div>
+                    </div>
+                    <div class="right-card-bottom-right">
+                        <button class="btn-like"><i class="fa-solid fa-heart"></i> Save</button>
+                        <button class="btn-delete"><i class="fa-solid fa-trash"></i> Delete</button>
+                    </div>
+                </div>
+            </div>
+         </div>`
+    });
+    leftCart.insertAdjacentHTML("beforeend",html);
+}
